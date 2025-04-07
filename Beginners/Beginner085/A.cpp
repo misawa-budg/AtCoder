85e98758ba22960f7e29d020642444e332a0cc51
misawa-budg <hmisawa426@keio.jp>
2025-04-07 21:09:15 +0900

    solve: Beginner085 ABC

---
+++ Beginners/Beginner085/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string S;
+    cin >> S;
+    S[3] = '8';
+
+    cout << S << endl;
+
+    return 0;
+}
