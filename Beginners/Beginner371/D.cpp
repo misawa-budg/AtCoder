0ba283e97721659848be1050ff9372055b05a977
misawa-budg <hmisawa426@keio.jp>
2025-04-09 20:59:46 +0900

    edit: Beginner371 D

--- Beginners/Beginner371/D.cpp
+++ Beginners/Beginner371/D.cpp
@@ -8,29 +8,30 @@ int main()
 
     int N;
     cin >> N;
-
-    vector<int> coordinates(N);
-    vector<long long> members(N);
-    for (int i = 0; i < N; i++) cin >> coordinates[i];
-    for (int i = 0; i < N; i++) cin >> members[i];
-
-    vector<long long> dp(N + 1, 0);
-    for (int i = 1; i <= N; i++) {
-        dp[i] = dp[i - 1] + members[i - 1];
-    }
+    vector<int> villages(N);
+    vector<long long> populations(N);
+    for (int i = 0; i < N; i++) cin >> villages[i];
+    for (int i = 0; i < N; i++) cin >> populations[i];
+    vector<long long> populationDp(N, 0LL);
+    populationDp[0] = populations[0];
+    for (int i = 1; i < N; i++) populationDp[i] = populationDp[i-1] + populations[i];
 
     int Q;
     cin >> Q;
-    for (int i = 0; i < Q; i++) {
-        int left, right;
-        cin >> left >> right;
-        int left_index = lower_bound(coordinates.begin(), coordinates.end(), left) - coordinates.begin();
-        int right_index = upper_bound(coordinates.begin(), coordinates.end(), right) - coordinates.begin() - 1;
 
-        if (left_index > right_index) {
+    for (int i = 0; i < Q; i++) {
+        int L, R;
+        cin >> L >> R;
+
+        int leftVillage = lower_bound(villages.begin(), villages.end(), L) - villages.begin();
+        int rightVillage = upper_bound(villages.begin(), villages.end(), R) - villages.begin() - 1;
+
+        if (rightVillage < leftVillage) {
             cout << 0 << endl;
+        } else if (leftVillage == rightVillage) {
+            cout << populations[leftVillage] << endl;
         } else {
-            long long ans = dp[right_index + 1] - dp[left_index];
+            long long ans = populationDp[rightVillage] - ((leftVillage == 0) ? 0 : populationDp[leftVillage - 1]);
             cout << ans << endl;
         }
     }
