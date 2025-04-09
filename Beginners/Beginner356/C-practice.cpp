d7c7a9b51e95cb0af2421136e69f749b56b4ae32
misawa-budg <hmisawa426@keio.jp>
2025-04-09 15:03:56 +0900

    edit: Beginner356 C-practice.cpp

--- Beginners/Beginner356/C-practice.cpp
+++ Beginners/Beginner356/C-practice.cpp
@@ -10,39 +10,39 @@ int main()
     cin >> N >> M >> K;
     vector<int> keys(M, 0);
     vector<int> results(M, 0);
-
     for (int i = 0; i < M; i++) {
-        int keyCount;
-        cin >> keyCount;
-        for (int j = 0; j < keyCount; j++) {
+        int keyNum;
+        cin >> keyNum;
+        for (int j = 0; j < keyNum; j++) {
             int key;
             cin >> key;
             key--;
             keys[i] |= (1 << key);
         }
+
         char result;
         cin >> result;
-        results[i] = (result == 'o') ?  1 : 0;
+        results[i] = (result == 'o') ? 1 : 0;
     }
 
-    int patterns = 0;
+    int ans = 0;
     for (int bit = 0; bit < (1 << N); bit++) {
         bool judge = true;
         for (int i = 0; i < M; i++) {
-            int trueKeysCount = __builtin_popcount(bit & keys[i]);
-            if (trueKeysCount >= K && !results[i]) {
+            int matchKeyCount = __builtin_popcount(bit & keys[i]);
+            if (matchKeyCount >= K && results[i] == 0) {
                 judge = false;
                 break;
             }
-            if (trueKeysCount < K && results[i]) {
+            if (matchKeyCount < K && results[i] == 1) {
                 judge = false;
                 break;
             }
         }
-        if (judge) patterns++;
+        if (judge) ans++;
     }
 
-    cout << patterns << endl;
+    cout << ans << endl;
 
     return 0;
 }
