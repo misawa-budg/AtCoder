137d0182729c532645518f69c64e36e2cf70413b
misawa-budg <hmisawa426@keio.jp>
2025-04-26 16:44:40 +0900

    solve: Beginner325 ABC

---
+++ Beginners/Beginner325/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string S, T; cin >> S >> T;
+
+    cout << S + " " + "san" << endl;
+
+    return 0;
+}
