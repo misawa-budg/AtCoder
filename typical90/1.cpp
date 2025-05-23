479084ef8232f0a359c4d2ee6a424ef44c6fec0a
misawa-budg <hmisawa426@keio.jp>
2025-05-23 18:07:49 +0900

    solve: Typical90 1

--- typical90/1.cpp
+++ typical90/1.cpp
@@ -1,11 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int N, L, K;
+int piece;
+bool check(int x, vector<int>& A)
+{
+    int piece = 0, prev = 0;
+    for (int i = 0; i < N; i++) {
+        if (A[i] - prev >= x) {
+            piece++;
+            prev = A[i];
+        }
+    }
+
+    if (L - prev >= x) piece++;
+
+    return piece >= K + 1;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
+    cin >> N >> L >> K;
+    vector<int> A(N); for (int i = 0; i < N; i++) cin >> A[i];
+
+    int lo = 0, hi = L;
+    while (hi - lo > 1) {
+        int mid = (lo + hi) / 2;
+        piece = 0;
+        if (check(mid, A)) {
+            lo = mid; 
+        } else {
+            hi = mid;
+        }
+    }
+
+    cout << lo << endl;
 
     return 0;
 }
