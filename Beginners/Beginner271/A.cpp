81bd4dcc85e2d150e2ea9fa3417ee4ee7004ad6f
misawa-budg <hmisawa426@keio.jp>
2025-06-12 15:48:31 +0900

    solve: Beginner271 ABC

---
+++ Beginners/Beginner271/A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N; cin >> N;
+
+    cout << hex << setfill('0') << setw(2) << uppercase << N << endl;
+
+    return 0;
+}
