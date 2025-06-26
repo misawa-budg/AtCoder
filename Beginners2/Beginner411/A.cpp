1ae5354d4e68c3eaec5ddc81efde0aaa3436d412
misawa-budg <hmisawa426@keio.jp>
2025-06-26 22:33:32 +0900

    initialize Beginner2 && solve: Beginner2 411 ABC

---
+++ Beginners2/Beginner411/A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string P; cin >> P;
+    int L; cin >> L;
+
+    cout << (P.size() >= L ? "Yes" : "No") << '\n';
+
+    return 0;
+}
