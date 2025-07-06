9eec57f6ff3a0cf25a265ae0161ce9565bfa884d
misawa-budg <hmisawa426@keio.jp>
2025-07-06 15:45:05 +0900

    refactor: Beginners 413 AD

--- Beginners2/Beginner413/A.cpp
+++ Beginners2/Beginner413/A.cpp
@@ -7,16 +7,12 @@ int main()
     cin.tie(nullptr);
 
     int N, M; cin >> N >> M;
-    vector<int> A(N); for (int i = 0; i < N; i++) cin >> A[i];
-
     for (int i = 0; i < N; i++) {
-        M -= A[i];
-        if (M < 0) {
-            cout << "No\n";
-            return 0;
-        }
+        int a; cin >> a;
+        M -= a;
     }
-    cout << "Yes\n";
+
+    cout << ((M < 0) ? "No" : "Yes") << endl;
 
     return 0;
 }
