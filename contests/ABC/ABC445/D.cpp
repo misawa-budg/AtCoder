2ebcca50c6156e6a0944af02fabad18ab42cf489
misawa-budg <hmisawa426@keio.jp>
2026-02-16 22:24:28 +0900

    retry: ABC445 D 1

--- contests/ABC/ABC445/D.cpp
+++ contests/ABC/ABC445/D.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
+int main() 
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -14,9 +14,9 @@ int main()
 
     vector<int> ord_h(N), ord_w(N);
     iota(ord_h.begin(), ord_h.end(), 0);
-    sort(ord_h.begin(), ord_h.end(), [&](int x, int y) {return h[x] > h[y];});
+    sort(ord_h.begin(), ord_h.end(), [&](int x, int y) { return h[x] > h[y]; });
     iota(ord_w.begin(), ord_w.end(), 0);
-    sort(ord_w.begin(), ord_w.end(), [&](int x, int y) {return w[x] > w[y];});
+    sort(ord_w.begin(), ord_w.end(), [&](int x, int y) { return w[x] > w[y]; });
 
     vector<int> ans_x(N), ans_y(N);
     vector<bool> used(N, false);
@@ -26,16 +26,15 @@ int main()
         while (used[*ith]) ith++;
         while (used[*itw]) itw++;
 
-        int i = (h[*ith] == H) ? *ith : *itw;
-        used[i] = true;
+        int piece = (h[*ith] == H) ? *ith : *itw;
+        used[piece] = true;
+        ans_x[piece] = H - h[piece] + 1;
+        ans_y[piece] = W - w[piece] + 1;
 
-        ans_x[i] = H - h[i] + 1;
-        ans_y[i] = W - w[i] + 1;
-        
-        if (h[i] == H) {
-            W -= w[i];
+        if (h[piece] == H) {
+            W -= w[piece];
         } else {
-            H -= h[i];
+            H -= h[piece];
         }
     }
 
