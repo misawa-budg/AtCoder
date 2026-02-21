477f40b2d97868b475e53f96418662dd5ce7e7fa
misawa-budg <hmisawa426@keio.jp>
2026-02-21 14:12:33 +0900

    retry: ABC445 C 1

--- contests/ABC/ABC445/C.cpp
+++ contests/ABC/ABC445/C.cpp
@@ -7,23 +7,21 @@ int main()
     cin.tie(nullptr);
 
     int N; cin >> N;
-    vector<int> A(N);
+    vector<int> A(N); 
     for (int i = 0; i < N; i++) {
         cin >> A[i];
         A[i]--;
     }
 
-    vector<int> ans(N); 
+    vector<int> ans(N);
     for (int i = N - 1; i >= 0; i--) {
-        if (A[i] == i) {
-            ans[i] = i;
-        } else {
-            ans[i] = ans[A[i]];
-        }
+        if (A[i] == i) ans[i] = i;
+        else ans[i] = ans[A[i]];
     }
 
-    for (int i = 0; i < N; i++) {
-        cout << ans[i] + 1 << ' ';
+    for (int i = 0; i < ans.size(); i++) {
+        if (i) cout << ' ';
+        cout << ans[i] + 1;
     } cout << '\n';
 
     return 0;
