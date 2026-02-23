e3a16130d787f63a7d560ce1d2b0d6ab340816ec
misawa-budg <hmisawa426@keio.jp>
2026-02-23 20:05:05 +0900

    retry: ABC446 C 1

--- contests/ABC/ABC446/C.cpp
+++ contests/ABC/ABC446/C.cpp
@@ -9,34 +9,18 @@ int main()
     int T; cin >> T;
     while (T--) {
         int N, D; cin >> N >> D;
-
         vector<int> A(N), B(N);
         for (int i = 0; i < N; i++) cin >> A[i];
         for (int i = 0; i < N; i++) cin >> B[i];
-        
-        deque<pair<long long, int>> eggs;
 
+        queue<int> q;
         for (int i = 0; i < N; i++) {
-            eggs.emplace_back(A[i], i);
-
-            long long need = B[i];
-            while (need > 0) {
-                auto &front = eggs.front();
-                long long take = min(need, front.first);
-                front.first -= take;
-                need -= take;
-                if (front.first == 0) eggs.pop_front();
-            }
-
-            while (!eggs.empty() && i - eggs.front().second >= D) {
-                eggs.pop_front();
-            }
+            for (int j = 0; j < A[i]; j++) q.push(i);
+            for (int j = 0; j < B[i]; j++) q.pop();
+            while (!q.empty() && i - q.front() >= D) q.pop();
         }
         
-        long long total = 0;
-        for (auto &egg : eggs) total += egg.first;
-        cout << total << '\n';
-
+        cout << q.size() << '\n';
     }
 
     return 0;
