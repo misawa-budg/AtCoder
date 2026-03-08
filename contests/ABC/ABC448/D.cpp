4c6b9445cdddd9174b99a93704f5b799793c457d
misawa-budg <hmisawa426@keio.jp>
2026-03-08 11:37:47 +0900

    retry: ABC448 D 1

--- contests/ABC/ABC448/D.cpp
+++ contests/ABC/ABC448/D.cpp
@@ -7,33 +7,40 @@ int main()
     cin.tie(nullptr);
 
     int N; cin >> N;
-    vector<int> A(N);
+    vector<int> A(N); 
+    vector<int> val;
     for (int i = 0; i < N; i++) {
         cin >> A[i];
+        val.push_back(A[i]);
     }
 
-    vector<vector<int>> adj(N);
-    for (int i = 0; i < N - 1; i++) {
+    sort(val.begin(), val.end());
+    val.erase(unique(val.begin(), val.end()), val.end());
+    for (int i = 0; i < N; i++) {
+        A[i] = lower_bound(val.begin(), val.end(), A[i]) - val.begin();
+    }
+    
+    vector<int> count(val.size(), 0);
+    vector<vector<int>> G(N);
+    for (int i = 0; i < N-1; i++) {
         int u, v; cin >> u >> v; u--; v--;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        G[u].push_back(v);
+        G[v].push_back(u);
     }
 
-    map<int, int> count;
-    int kind = 1;
-    vector<bool> ans(N);
-
+    vector<bool> ans(N, false);
+    int dup = 0;
     auto dfs = [&](auto& self, int u, int p) -> void {
         count[A[u]]++;
-        if (count[A[u]] == 2) kind++;
+        if (count[A[u]] == 2) dup++;
 
-        ans[u] = (kind > 1);
+        ans[u] = (dup > 0);
 
-        for (int v : adj[u]) {
+        for (int v : G[u]) {
             if (v != p) self(self, v, u);
         }
 
-        if (count[A[u]] == 2) kind--;
+        if (count[A[u]] == 2) dup--;
         count[A[u]]--;
     };
 
