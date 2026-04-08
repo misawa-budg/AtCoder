03f5137844b13ee272e09eccd76ac57aba41cab9
misawa-budg <hmisawa426@keio.jp>
2026-04-08 12:58:21 +0900

    retry: ABC437 D

--- contests/ABC/ABC437/D.cpp
+++ contests/ABC/ABC437/D.cpp
@@ -6,26 +6,26 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    const long long MOD = 998244353;
     int N, M; cin >> N >> M;
     vector<long long> A(N), B(M);
     for (int i = 0; i < N; i++) cin >> A[i];
     for (int i = 0; i < M; i++) cin >> B[i];
-
     sort(A.begin(), A.end());
-    vector<long long> psA(N+1, 0);
+
+    vector<long long> psA(N + 1, 0);
     for (int i = 0; i < N; i++) psA[i + 1] = psA[i] + A[i];
 
-    long long res = 0;
+    const long long MOD = 998244353;
+    long long result = 0;
     for (int i = 0; i < M; i++) {
         long long target = B[i];
-        int border = lower_bound(A.begin(), A.end(), target) - A.begin();
-        long long lower = (border * target - psA[border] + MOD) % MOD;
-        long long upper = ((psA[N] - psA[border]) - target * (N - border) + MOD) % MOD;
-        res = (res + lower + upper) % MOD;
+        int border /* target以下の数 */ = lower_bound(A.begin(), A.end(), target) - A.begin();
+        long long lower = (target * border - psA[border] + MOD) % MOD; 
+        long long upper = (psA[N] - psA[border] - target * (N - border) + MOD) % MOD;
+        result = (result + lower + upper) % MOD;
     }
 
-    cout << res << '\n';
+    cout << result << '\n';
 
     return 0;
 }
