e1c3e6b1d5239b111bd3eadaae66d0827244cfc4
misawa-budg <hmisawa426@keio.jp>
2026-04-09 16:41:02 +0900

    retry: ABC438 D

--- contests/ABC/ABC438/D.cpp
+++ contests/ABC/ABC438/D.cpp
@@ -1,8 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -12,35 +11,17 @@ int main()
     for (int i = 0; i < N; i++) cin >> B[i];
     for (int i = 0; i < N; i++) cin >> C[i];
 
-    // vector<long long> psA(N + 1, 0), psB(N + 1, 0), psC(N + 1, 0);
-    // for (int i = 0; i < N; i++) {
-    //     psA[i+1] = psA[i] + A[i];
-    //     psB[i+1] = psB[i] + B[i];
-    //     psC[i+1] = psC[i] + C[i];
-    // }
+    vector<vector<long long>> dp(N + 1, vector<long long>(3, LLONG_MIN));
 
-    // long long ans = LLONG_MIN;
-    // long long Amax = LLONG_MIN;
-
-    // for (int y = 2; y < N; y++) {
-    //     int x = y - 1;
-    //     Amax = max(Amax, psA[x] - psB[x]);
-    //     ans = max(ans, Amax + psB[y] - psC[y] + psC[N]);
-    // }
-
-    // cout << ans << '\n';
-    
-    vector<vector<long long>> dp(N, vector<long long>(3, LLONG_MIN));
-    dp[0][0] = A[0];
-    for (int i = 1; i < N; i++) {
-        if (dp[i-1][0] != LLONG_MIN) 
-            dp[i][0] = dp[i-1][0] + A[i];
-        if (dp[i-1][0] != LLONG_MIN || dp[i-1][1] != LLONG_MIN)
-            dp[i][1] = max(dp[i-1][0], dp[i-1][1]) + B[i];
-        if (dp[i-1][1] != LLONG_MIN || dp[i-1][2] != LLONG_MIN)
-            dp[i][2] = max(dp[i-1][1], dp[i-1][2]) + C[i];
+    dp[1][0] = A[0];
+    for (int i = 2; i <= N; i++) {
+        long long a = A[i-1], b = B[i-1], c = C[i-1];
+        dp[i][0] = dp[i-1][0] + a;
+        dp[i][1] = max(dp[i-1][1], dp[i-1][0]) + b;
+        dp[i][2] = max(dp[i-1][1], dp[i-1][2]) + c;
     }
-    cout << dp[N-1][2] << '\n';
+
+    cout << dp[N][2] << '\n';
 
     return 0;
 }
